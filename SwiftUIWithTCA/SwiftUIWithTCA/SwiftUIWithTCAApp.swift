ff386da1142e75e45667c793f75e3ebefb75bd69
Jayfunf <cho.minhyun@cashwalk.io>
2023-08-19 22:37:29 +0900

    Init

---
+++ SwiftUIWithTCA/SwiftUIWithTCA/SwiftUIWithTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWithTCAApp.swift
+//  SwiftUIWithTCA
+//
+//  Created by Minhyun Cho on 2023/08/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWithTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
